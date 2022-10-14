40dd4c52a69347adbdce98d7d979959d8f0a8f0f
杨勇 <1994101600@qq.com>
2022-10-14 11:44:34 +0800

    feat: check package name available

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
 import minimist from 'minimist'
 import fetch from 'node-fetch'
+import open from 'open'
 import log from './utils/log'
+
+const isAvaliablePackage = async (name: string): Promise<boolean> => {
+  const response = await fetch(`https://api.npms.io/v2/search?${new URLSearchParams({
+    q: name,
+    size: '1',
+  })}`, {
+    method: 'GET',
+  })
+  const data = await response.json()
+  const { total = 0, results = [] } = data as any
+  if (total > 1 || results.length)
+    return true
+
+  return false
+}
+
+const visitCnpmSyncWebsite = async (pkgName: string) => {
+  const url = `https://npmmirror.com/sync/${pkgName}`
+
+  open(url)
+
+  // fake success
+  setTimeout(() => {
+    log.success(`sync ${pkgName} complete!`)
+  }, 2000)
+}
 
 const init = async () => {
   const argv = minimist(process.argv.slice(2), {})
@@ -12,13 +39,10 @@ const init = async () => {
 
   const pkgName = args[0]
 
-  await fetch(`https://npmmirror.com/sync/${pkgName}`, { method: 'GET' })
-  // const data = await response.json()
+  if (!(await isAvaliablePackage(pkgName)))
+    throw new Error('invalid package name')
 
-  // fake success
-  setTimeout(() => {
-    log.success(`sync ${pkgName} complete!`)
-  }, 2000)
+  visitCnpmSyncWebsite(pkgName)
 }
 
 init().catch((e) => {
