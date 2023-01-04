3644fcf4b04bd71bdf005551ef8788f031ab47ba
杨勇 <1994101600@qq.com>
2023-01-04 15:40:57 +0800

    fix: npm serach package api

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,30 @@ import fetch from 'node-fetch'
 import open from 'open'
 import log from './utils/log'
 
-const isAvaliablePackage = async (name: string): Promise<boolean> => {
-  const response = await fetch(`https://api.npms.io/v2/search?${new URLSearchParams({
+const isAvaliablePackage = async (name: string): Promise<boolean | string> => {
+  const response = await fetch(`https://www.npmjs.com/search/suggestions?${new URLSearchParams({
     q: name,
-    size: '1',
   })}`, {
     method: 'GET',
   })
-  const data = await response.json()
-  const { total = 0, results = [] } = data as any
-  if (total > 1 || results.length)
-    return true
+  const data = await response.json() as any[]
+  if (data?.length) {
+    const pkg = data.find(item => item.name === name)
+    if (pkg)
+      return pkg.version
+  }
 
   return false
 }
 
-const visitCnpmSyncWebsite = async (pkgName: string) => {
+const visitCnpmSyncWebsite = async (pkgName: string, version?: string) => {
   const url = `https://npmmirror.com/sync/${pkgName}`
 
   open(url)
 
   // fake success
   setTimeout(() => {
-    log.success(`sync ${pkgName} complete!`)
+    log.success(`sync ${pkgName} complete! the latest version is v${version}`)
   }, 2000)
 }
 
@@ -39,10 +40,11 @@ const init = async () => {
 
   const pkgName = args[0]
 
-  if (!(await isAvaliablePackage(pkgName)))
-    throw new Error('invalid package name')
+  const latestVersion = await isAvaliablePackage(pkgName)
+  if (!latestVersion)
+    throw new Error(`${pkgName} is invalid package name`)
 
-  visitCnpmSyncWebsite(pkgName)
+  visitCnpmSyncWebsite(pkgName, latestVersion)
 }
 
 init().catch((e) => {
